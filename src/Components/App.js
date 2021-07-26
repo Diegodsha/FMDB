184b2f3a13e2aa4c268214c932e3de7c52f76fc0
Diego <diegodsha@hotmail.com>
2021-07-25 20:22:16 -0500

    Add carousel

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,19 @@
+// import { useDispatch } from 'react-redux';
+// import { useEffect } from 'react';
+// import { fetchMovies } from '../Actions/index';
 import MovieList from '../Containers/MovieList';
+import Carousel from './Carousel';
 import Nav from './Nav';
 
 function App() {
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(fetchMovies());
+  // }, []);
   return (
     <div className="App">
       <Nav />
+      <Carousel />
       <MovieList />
     </div>
   );
---
+++ src/Components/Carousel.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-unused-vars */
+import { useSelector } from 'react-redux';
+
+const Carousel = () => {
+  const mostPopularMovies = useSelector((state) => state.moviesReducer[0]);
+
+  return (
+    <div
+      id="carouselExampleCaptions"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        />
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        />
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        />
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="https://4.bp.blogspot.com/-4OF9Rh-W3Ec/XwTgPMKlHQI/AAAAAAAAGKU/GMIBAnNPKycov2S4EC-7lf9EZpELbZm5gCK4BGAYYCw/s1600/BlackWidow_Wallpaper_2020_MarvelSpoilerOficial.png" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://cdn.wallpapersafari.com/10/0/rn0iSv.jpg" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src="https://dove.org/wp-content/uploads/tomorrow6.jpeg" className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true" />
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true" />
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
