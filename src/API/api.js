852c5c18dd6ab1abb16e9006b643c59e9509e349
Diego <diegodsha@hotmail.com>
2021-07-25 20:21:21 -0500

    Use promise all to fetch movies

--- src/API/api.js
+++ src/API/api.js
@@ -1,16 +1,27 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable no-unused-vars */
 import axios from 'axios';
+import { moviesEndPoint, seriesEndPoint } from './Endpoints';
 
 const API_KEY = 'k_8v3xbv31';
-const BASE_URL = 'https://imdb-api.com/en/API/MostPopularMovies/';
+const { mostPopular, top250, inTheaters, comingSoon } = moviesEndPoint;
+const { mostPopularS, top250S } = seriesEndPoint;
 
-const getMovies = async () => {
-  const res = await axios.get(`${BASE_URL}${API_KEY}`);
-  return res.data;
+const getMostPopularMovies = async () => {
+  const res = await axios.get(`${mostPopular}${API_KEY}`);
+  return res.data.items;
 };
+
+const getTop250Movies = async () => {
+  const res = await axios.get(`${top250}${API_KEY}`);
+  return res.data.items;
+};
+
+const allMovies = Promise.all([getMostPopularMovies(), getTop250Movies()]).then((res) => res);
 
 const getSeries = async () => {
-  const res = await axios.get(`${BASE_URL}${API_KEY}`);
+  const res = await axios.get(`${mostPopularS}${API_KEY}`);
   return res.data;
 };
 
-export { getMovies, getSeries };
+export { getMostPopularMovies, getTop250Movies, getSeries, allMovies };
--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,11 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { getMostPopularMovies, getTop250Movies, allMovies } from '../API/api';
 import types from './Types';
 
-export const fetchMovies = (movies) => ({
+export const fetchMoviesAction = (movies) => ({
   type: types.fetchMovies,
   movies,
 });
 
-export const fetchSeries = (series) => ({
+export const fetchSeriesAction = (series) => ({
   type: types.fetchSeries,
   series,
 });
+
+export const fetchMovies = () => async (dispatch) => {
+  const movies = await allMovies;
+  //   console.log(movies);
+  dispatch(fetchMoviesAction(movies));
+};
