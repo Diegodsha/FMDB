d5323e0a19ce060d844b4cce6f7404630c5f0b58
Diego <diegodsha@hotmail.com>
2021-07-25 20:22:03 -0500

    Render most popular movies

--- src/Containers/MovieList.js
+++ src/Containers/MovieList.js
@@ -1,7 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchMovies } from '../Actions/index';
 import List from '../Styles/List.css';
 
-const MovieList = () => (
-  <h3 className={List.h3}>MovieList</h3>
-);
+const MovieList = () => {
+  const mostPopularMovies = useSelector((state) => state.moviesReducer[0]);
+  const dispatch = useDispatch();
+  console.log('redner');
+
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, []);
+
+  return (
+    <>
+      <h3 className={List.h3}>MovieList</h3>
+      <div>
+        {mostPopularMovies?.map((movie) => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default MovieList;
