53dcfeddcf4d4dcf94c4487643145befd9c764b9
Diego <diegodsha@hotmail.com>
2021-07-25 20:20:29 -0500

    Add movies/series endpoints

---
+++ src/API/Endpoints.js
@@ -0,0 +1,13 @@
+const moviesEndPoint = {
+  mostPopular: 'https://imdb-api.com/en/API/MostPopularMovies/',
+  top250: 'https://imdb-api.com/en/API/Top250Movies/',
+  inTheaters: 'https://imdb-api.com/en/API/InTheaters/',
+  comingSoon: 'https://imdb-api.com/en/API/ComingSoon/',
+};
+
+const seriesEndPoint = {
+  mostPopularS: 'https://imdb-api.com/en/API/MostPopularTVs/',
+  top250S: 'https://imdb-api.com/en/API/Top250TVs/',
+};
+
+export { moviesEndPoint, seriesEndPoint };
