9eef40752c9aa8473f52d948d7ac0d738a2b7295
Diego <diegodsha@hotmail.com>
2021-07-27 12:01:26 -0500

    Add details link to card

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,33 +1,47 @@
 /* eslint-disable camelcase */
 /* eslint-disable object-curly-newline */
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Card from '../Styles/Card.css';
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie, button }) => {
   // eslint-disable-next-line no-unused-vars
 
-  const { title, release_date, vote_average, poster_path } = movie;
+  const { title, release_date, vote_average, poster_path, id } = movie;
 
   return (
-    <div
-      className={`card bg-dark text-white ${Card.card}`}
-      style={{ width: '22%' }}
-    >
-      <img
-        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-        className="card-img"
-        alt={title}
-      />
+    <div className="col-5 col-md-3 col-lg-2">
       <div
-        className="card-img-overlay"
-        style={{ background: 'rgba(0, 0, 0, 0.356)' }}
+        className={`card bg-dark text-white ${Card.card}`}
+        // style={{ width: '22%' }}
       >
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{release_date}</p>
-        <p className="card-text">{vote_average}</p>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+          className="card-img"
+          alt={title}
+        />
+        <div
+          className="card-img-overlay"
+          style={{ background: 'rgba(0, 0, 0, 0.356)' }}
+        >
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{release_date}</p>
+          <p className="card-text">{vote_average}</p>
+          {button === 'yes' ? (
+            <Link to={`details/${id}`} style={{ zIndex: '2' }}>
+              <button className="btn btn-primary btn-sm" type="button">
+                See more
+              </button>
+            </Link>
+          ) : null}
+        </div>
       </div>
     </div>
   );
+};
+
+MovieCard.defaultProps = {
+  button: 'no',
 };
 
 MovieCard.propTypes = {
@@ -36,7 +50,9 @@ MovieCard.propTypes = {
     release_date: PropTypes.string.isRequired,
     vote_average: PropTypes.number.isRequired,
     poster_path: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
   }).isRequired,
+  button: PropTypes.string,
 };
 
 export default MovieCard;
