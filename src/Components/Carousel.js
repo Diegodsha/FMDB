b74f90d38bb887a2f428e2b5c16aa97ecd33fc00
Diego <diegodsha@hotmail.com>
2021-07-27 11:59:46 -0500

    Add state to carousel component

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,13 +1,22 @@
 /* eslint-disable no-unused-vars */
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../API/api';
+import { moviesEndPoint } from '../API/Endpoints';
+import StyledCarousel from '../Styles/Carrousel.module.css';
 
 const Carousel = () => {
-  const mostPopularMovies = useSelector((state) => state.moviesReducer[0]);
+  const [FivePopularMovies, setFivePopularMovies] = useState([]);
+  useEffect(() => {
+    axios.get(`${BASE_URL}${moviesEndPoint.mostPopular}`).then((res) => {
+      setFivePopularMovies(res.data.results.slice(0, 5));
+    });
+  }, []);
 
   return (
     <div
       id="carouselExampleCaptions"
-      className="carousel slide"
+      className={`carousel slide ${StyledCarousel.margin}`}
       data-bs-ride="carousel"
     >
       <div className="carousel-indicators">
@@ -31,29 +40,36 @@ const Carousel = () => {
           data-bs-slide-to="2"
           aria-label="Slide 3"
         />
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="3"
+          aria-label="Slide 4"
+        />
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="4"
+          aria-label="Slide 5"
+        />
       </div>
-      <div className="carousel-inner">
+      <div className={`carousel-inner ${StyledCarousel.rounded}`}>
         <div className="carousel-item active">
-          <img src="https://4.bp.blogspot.com/-4OF9Rh-W3Ec/XwTgPMKlHQI/AAAAAAAAGKU/GMIBAnNPKycov2S4EC-7lf9EZpELbZm5gCK4BGAYYCw/s1600/BlackWidow_Wallpaper_2020_MarvelSpoilerOficial.png" className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>
+          <img
+            src={`https://image.tmdb.org/t/p/original${FivePopularMovies[0]?.poster_path}`}
+            className={`d-block w-100 ${StyledCarousel.fit}`}
+            alt="..."
+          />
         </div>
-        <div className="carousel-item">
-          <img src="https://cdn.wallpapersafari.com/10/0/rn0iSv.jpg" className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
+        {FivePopularMovies.slice(1)?.map((item) => (
+          <div key={item.id} className="carousel-item">
+            <img
+              src={`https://image.tmdb.org/t/p/original${item?.poster_path}`}
+              className={`d-block w-100 ${StyledCarousel.fit}`}
+              alt={item?.title}
+            />
           </div>
-        </div>
-        <div className="carousel-item">
-          <img src="https://dove.org/wp-content/uploads/tomorrow6.jpeg" className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
+        ))}
       </div>
       <button
         className="carousel-control-prev"
